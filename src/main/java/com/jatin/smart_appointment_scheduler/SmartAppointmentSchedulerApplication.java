dcd56ca9e1cbe56b8c7954466042526089e90be2
Jatin Lanje <j3lanje@gmail.com>
2025-07-14 23:10:38 +0530

    feat: deal time in UTC

--- src/main/java/com/jatin/smart_appointment_scheduler/SmartAppointmentSchedulerApplication.java
+++ src/main/java/com/jatin/smart_appointment_scheduler/SmartAppointmentSchedulerApplication.java
@@ -3,9 +3,18 @@ package com.jatin.smart_appointment_scheduler;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SmartAppointmentSchedulerApplication {
+
+	@PostConstruct
+	void setUTCTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
