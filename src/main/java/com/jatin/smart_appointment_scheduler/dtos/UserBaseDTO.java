32cf6cdc9f8878675a39a717810fef3231af3d8c
Jatin Lanje <j3lanje@gmail.com>
2025-07-16 11:54:58 +0530

    fix: add null check annotation

--- src/main/java/com/jatin/smart_appointment_scheduler/dtos/UserBaseDTO.java
+++ src/main/java/com/jatin/smart_appointment_scheduler/dtos/UserBaseDTO.java
@@ -3,6 +3,7 @@ package com.jatin.smart_appointment_scheduler.dtos;
 import com.jatin.smart_appointment_scheduler.enums.Gender;
 import com.jatin.smart_appointment_scheduler.enums.Role;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -13,14 +14,14 @@ public abstract class UserBaseDTO {
     private String firstName;
     @NotBlank(message = "Last name is mandatory")
     private String lastName;
-    //@NotBlank(message = "Gender is mandatory")
+    @NotNull(message = "Gender is mandatory")
     private Gender gender;
     @NotBlank(message = "Phone and Email is mandatory")
     private String email, phone;
-    //@NotBlank(message = "DOB is mandatory")
+    @NotNull(message = "DOB is mandatory")
     private LocalDate dob;
     @NotBlank(message = "Password is mandatory")
     private String password;
-    //@NotBlank(message = "Role is mandatory")
+    @NotNull(message = "Role is mandatory")
     private Role role;
 }
