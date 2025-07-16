c08e81a37d89061c47e77b3ba848683b12305c9f
Jatin Lanje <j3lanje@gmail.com>
2025-07-16 11:50:36 +0530

    Revert "fix: add null check annotation"
    This reverts commit a8e71285222d31d23b0803ccd8e769f9911c2787.

--- src/main/java/com/jatin/smart_appointment_scheduler/dtos/UserBaseDTO.java
+++ src/main/java/com/jatin/smart_appointment_scheduler/dtos/UserBaseDTO.java
@@ -3,7 +3,6 @@ package com.jatin.smart_appointment_scheduler.dtos;
 import com.jatin.smart_appointment_scheduler.enums.Gender;
 import com.jatin.smart_appointment_scheduler.enums.Role;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -14,14 +13,14 @@ public abstract class UserBaseDTO {
     private String firstName;
     @NotBlank(message = "Last name is mandatory")
     private String lastName;
-    @NotNull(message = "Gender is mandatory")
+    //@NotBlank(message = "Gender is mandatory")
     private Gender gender;
     @NotBlank(message = "Phone and Email is mandatory")
     private String email, phone;
-    @NotNull(message = "DOB is mandatory")
+    //@NotBlank(message = "DOB is mandatory")
     private LocalDate dob;
     @NotBlank(message = "Password is mandatory")
     private String password;
-    @NotNull(message = "Role is mandatory")
+    //@NotBlank(message = "Role is mandatory")
     private Role role;
 }
