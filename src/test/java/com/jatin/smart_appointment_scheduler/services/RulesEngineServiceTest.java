092998954ee662469788edc4e77094512d4b0255
Jatin Lanje <j3lanje@gmail.com>
2025-07-19 20:18:49 +0530

    test: add unit test template for RulesEngineService

---
+++ src/test/java/com/jatin/smart_appointment_scheduler/services/RulesEngineServiceTest.java
@@ -0,0 +1,20 @@
+package com.jatin.smart_appointment_scheduler.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class RulesEngineServiceTest {
+    @InjectMocks
+    private RulesEngineService rulesEngineService;
+
+    // Mock dependencies here
+
+    @Test
+    void sampleTest() {
+        // TODO: implement test
+    }
+} 
